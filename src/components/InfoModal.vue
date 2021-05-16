53fa06f1fa3f9f61e07f48fa54bd1d14cc478d5b
pherzog <s6phherz@uni-bonn.de>
2021-05-16 05:26:29 +0200

    Added licenses to info modal

--- src/components/InfoModal.vue
+++ src/components/InfoModal.vue
@@ -6,10 +6,22 @@
     <div class="d-block text-left">
       <p>Diese Anwendung wurde im Sommersemster 2021 im Rahmen des Moduls Mensch-Computer-Interaktion an 
       der Universität Bonn entwickelt.</p>
-      <a href="https://philipp.herzo.gs/datenschutz">Datenschutz</a>&nbsp;
-      <a href="https://philipp.herzo.gs/impressum">Impressum</a>
+
+      <b-list-group>
+        <b-list-group-item><strong>Verwendete Projekte / Lizenzen</strong></b-list-group-item>
+        <b-list-group-item href="https://www.npmjs.com/package/vue-json-csv">vue-json-csv - MIT</b-list-group-item>
+        <b-list-group-item href="https://www.npmjs.com/package/vue-page-title">vue-page-title - MIT</b-list-group-item>
+        <b-list-group-item href="https://www.npmjs.com/package/bootstrap-icons">bootstrap-icons - MIT</b-list-group-item>
+        <b-list-group-item href="https://www.npmjs.com/package/bootstrap-vue">bootstrap-vue - MIT</b-list-group-item>
+        <b-list-group-item href="https://www.npmjs.com/package/core-js">core-js - MIT</b-list-group-item>
+        <b-list-group-item href="https://www.npmjs.com/package/vue">vue - MIT</b-list-group-item>
+      </b-list-group>
     </div>
     <template #modal-footer>
+      <div class="mr-1 text-left">
+        <a href="https://philipp.herzo.gs/datenschutz">Datenschutz</a>&nbsp;-&nbsp;
+        <a href="https://philipp.herzo.gs/impressum">Impressum</a>
+      </div>
       <b-button variant="dark" @click="$bvModal.hide('info-modal')">Schließen</b-button>
     </template>
   </b-modal>
